194c9bf31b597de486603f3a820e9a7f37b79de1
Renato <renato.matosinho.martins@gmail.com>
2020-07-09 01:57:25 -0300

    Class 08/07/2020
    Double jump mechanic.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,35 +6,85 @@ public class Movement : MonoBehaviour
 {
 	public float walkSpeed = 5;
 	public float jumpSpeed = 15;
+	public int maxJumps = 2;
+	public LayerMask groundLayers;
 
 	private Rigidbody2D _rigidbody2D;
+	private bool _isGrounded = false;
+	private int _remainingJumps = 0;
+	private float _leaveGroundTime = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+		// Get Rigidbody2D instance from current GameObject.
 		_rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+		// Read input from player (ONLY READS INPUT FROM ONE PLAYER).
 		bool isLeftPressed = Input.GetKey(KeyCode.A);
 		bool isRightPressed = Input.GetKey(KeyCode.D);
-		//bool isJumping = Input.GetKey(KeyCode.Space);
+		bool isJumping = Input.GetKeyDown(KeyCode.Space);
 
-		if (isLeftPressed)
-			_rigidbody2D.velocity = new Vector2(-walkSpeed, _rigidbody2D.velocity.y);
-		else if (isRightPressed)
-			_rigidbody2D.velocity = new Vector2(walkSpeed, _rigidbody2D.velocity.y);
+		// New velocity temporary vector.
+		Vector2 newVelocity = _rigidbody2D.velocity;
+
+		// Checks if character is grounded accounting for grace time when just jumped
+		if (_rigidbody2D.IsTouchingLayers(groundLayers) && 
+			(_leaveGroundTime + 0.2f) - Time.time < 0)
+		{
+			_isGrounded = true;
+			_remainingJumps = maxJumps;
+		}
 		else
-			_rigidbody2D.velocity = new Vector2(0, _rigidbody2D.velocity.y);
+			_isGrounded = false;
 
-		/*if (isJumping && _rigidbody2D.IsTouchingLayers(LayerMask.GetMask(new string[] { "Ground" })))
-			_rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, jumpSpeed);
-
-		if (!_rigidbody2D.IsTouchingLayers(LayerMask.GetMask(new string[] { "Ground" })))
-			_rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _rigidbody2D.velocity.y + Physics2D.gravity.y);
+		// Calculates horizontal movement.
+		if (isLeftPressed && !isRightPressed)
+			newVelocity.x = -walkSpeed;
+		else if (isRightPressed && !isLeftPressed)
+			newVelocity.x = walkSpeed;
 		else
-			_rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, 0);*/
+			newVelocity.x = 0;
+
+		// Set vertical velocity to zero if player is grounded
+		if (!isJumping && _isGrounded)
+			newVelocity.y = 0;
+		// When player press jump and on ground ,
+		// Set vertical velocity to jump speed,
+		// set time to check grace time and
+		// and decrement jumps remaining.
+		else
+		if (isJumping && _isGrounded)
+		{
+			newVelocity.y = jumpSpeed;
+			_remainingJumps--;
+
+			_leaveGroundTime = Time.time;
+		}
+		// When player press jump and not on ground and
+		// still has jumps remaining and starting to fall,
+		// Set vertical velocity to jump speed
+		// and decrement jumps remaining
+		// and apply gravity.
+		else
+		if (isJumping && 
+			!_isGrounded && 
+			_remainingJumps > 0 && 
+			newVelocity.y < -2.5f)
+		{
+			newVelocity.y = jumpSpeed;
+			_remainingJumps--;
+			newVelocity.y += Physics2D.gravity.y * Time.deltaTime;
+		}
+		// Apply gravity.
+		else
+			newVelocity.y += Physics2D.gravity.y * Time.deltaTime;
+
+		// Apply calculated velocity to character's Rigidbody2D
+		_rigidbody2D.velocity = newVelocity;
 	}
 }
