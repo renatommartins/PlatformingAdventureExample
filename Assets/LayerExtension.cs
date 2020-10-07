b2b0641ada677dacde577335a83752c14829103e
Renato <renato.matosinho.martins@gmail.com>
2020-10-07 20:21:57 -0300

    Class 04/08/2020

---
+++ Assets/LayerExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public static class LayerExtension
+{
+	public static bool ContainsLayer(this LayerMask layerMask, int layer)
+	{
+		return ((1 << layer) & layerMask.value) != 0;
+	}
+}
---
+++ Assets/LevelReset.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelReset : MonoBehaviour
+{
+	public LayerMask KillPlaneLayer;
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		foreach (ContactPoint2D point in collision.contacts)
+			if(KillPlaneLayer.ContainsLayer(point.collider.gameObject.layer))
+				UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+	}
+	
+}
